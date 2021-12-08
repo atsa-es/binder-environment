c5e2e51a760c50ec40ebfebb4b33bfa7870a6dc2
Eli Holmes <eli.holmes@noaa.gov>
2021-12-07 21:32:19 -0800

    Update install.R

--- install.R
+++ install.R
@@ -1,3 +1,5 @@
 # This will get all the needed packages
 install.packages("remotes")
 remotes::install_github("atsa-es/atsalibrary", dependencies = c("Depends", "Imports"))
+# This fixes a bug that didn't allow help to be shown in Jupyter notebooks
+remotes::install_github("IRkernel/repr")
