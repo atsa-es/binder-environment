2f1d1a8132128305e8a24472cee1f2c6d24150eb
Eli Holmes <eli.holmes@noaa.gov>
2021-12-07 20:46:37 -0800

    Update install.R

--- install.R
+++ install.R
@@ -1,3 +1,3 @@
 # This will get all the needed packages
 install.packages("remotes")
-remotes::install_github("atsa-es/atsalibrary", dependencies = c("Depends", "Imports"))
+#remotes::install_github("atsa-es/atsalibrary", dependencies = c("Depends", "Imports"))
